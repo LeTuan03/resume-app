48ee6e64320dd6da8b5dae4d21e84a46bc6e6d53
LeTuan03 <leetuan191003@gmail.com>
2023-08-28 08:08:25 +0700

    add model delete

---
+++ src/common/delete/ModalDelete.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { Button, Modal } from "antd";
+import { DeleteOutlined } from "@ant-design/icons";
+const ModalDelete = ({ onDelete }) => {
+    const [open, setOpen] = useState(false);
+    return (
+        <div
+            onClick={(e) => {
+                e.stopPropagation();
+            }}
+        >
+            <Button
+                icon={<DeleteOutlined />}
+                className="flex justify-center items-center w-full "
+                type="text"
+                onClick={(e) => {
+                    e.stopPropagation();
+                    setOpen(true);
+                }}
+            >
+                Delete
+            </Button>
+            <Modal
+                title="Confirm delete !"
+                centered
+                open={open}
+                footer={
+                    <>
+                        <Button onClick={onDelete}>Delete</Button>
+                        <Button
+                            className="bg-[#1677ff]"
+                            onClick={(e) => {
+                                e.stopPropagation();
+                                setOpen(false);
+                            }}
+                            type="primary"
+                        >
+                            Cancle
+                        </Button>
+                    </>
+                }
+            >
+                <p>Are you sure you want to delete this entry?</p>
+            </Modal>
+        </div>
+    );
+};
+export default ModalDelete;
--- src/components/personal-details/PersonalDetails.jsx
+++ src/components/personal-details/PersonalDetails.jsx
@@ -1,55 +1,32 @@
 import React, { useState } from "react";
 import { Avatar, Form, Input, Typography, Upload, Button } from "antd";
-import { DeleteOutlined, EditOutlined, UserOutlined } from "@ant-design/icons";
-
+import { EditOutlined, UserOutlined } from "@ant-design/icons";
 import { useDispatch } from "react-redux";
 const { Title } = Typography;
 
 import { add } from "../../redux/personal/personalSlice";
+import ModalDelete from "../../common/delete/ModalDelete";
+import { debounce } from "lodash";
 
 export default function PersonalDetails() {
     const dispatch = useDispatch();
     const [avata, setAvata] = useState("");
+    const [loading, setLoading] = useState(true);
 
     const onFinish = (value) => {
         value.avata = avata;
         dispatch(add(value));
     };
-    const handleBeforeUpload = (file) => {
+    const setDebouncedAvata = debounce((file) => {
         setAvata(URL.createObjectURL(file));
+        setLoading(false);
+    }, 1000);
+
+    const handleBeforeUpload = (file) => {
+        setLoading(true);
+        setDebouncedAvata(file);
         return false;
     };
-    const uploadButton = (
-        <div className="flex justify-center items-center">
-            {!avata && (
-                <Avatar
-                    shape="square"
-                    size={64}
-                    icon={
-                        <UserOutlined className="text-[#bec4d5] hover:text-[#1a91f0] flex justify-center items-center h-full" />
-                    }
-                    className="bg-[#eff2f9]"
-                />
-            )}
-            {avata && (
-                <div>
-                    <Button
-                        icon={<EditOutlined />}
-                        className="flex justify-center items-center"
-                        type="text"
-                    >
-                        Edit photo
-                    </Button>
-                </div>
-            )}
-            {!avata && (
-                <Button className="border-none text-[#1a91f0] shadow-none">
-                    <b>Upload photo</b>
-                </Button>
-            )}
-        </div>
-    );
-
     return (
         <div className="mb-5">
             <Title level={4} className="!mb-5">
@@ -71,7 +48,7 @@ export default function PersonalDetails() {
                         />
                     </Form.Item>
 
-                    <div className="flex items-center">
+                    <div className="flex items-center gap-3">
                         {avata && (
                             <img
                                 src={avata}
@@ -80,64 +57,108 @@ export default function PersonalDetails() {
                             />
                         )}
                         <Upload beforeUpload={handleBeforeUpload}>
-                            {uploadButton}
+                            {!avata ? (
+                                <>
+                                    <Avatar
+                                        shape="square"
+                                        size={64}
+                                        icon={
+                                            <UserOutlined className="text-[#bec4d5] hover:text-[#1a91f0] flex justify-center items-center h-full" />
+                                        }
+                                        className="bg-[#eff2f9]"
+                                    />
+                                    <Button
+                                        className="border-none text-[#1a91f0] shadow-none"
+                                        loading={loading}
+                                    >
+                                        <b>Upload photo</b>
+                                    </Button>
+                                </>
+                            ) : (
+                                <>
+                                    <div>
+                                        <Button
+                                            loading={loading}
+                                            icon={<EditOutlined />}
+                                            className="flex justify-center items-center w-full"
+                                            type="text"
+                                        >
+                                            Edit photo
+                                        </Button>
+                                    </div>
+                                    <ModalDelete
+                                        onDelete={(e) => {
+                                            e.stopPropagation();
+                                            setAvata("");
+                                        }}
+                                    />
+                                </>
+                            )}
                         </Upload>
-                        {avata && (
-                            <Button
-                                icon={<DeleteOutlined />}
-                                className="flex justify-center items-center"
-                                type="text"
-                                onClick={(e) => {
-                                    e.preventDefault();
-                                    setAvata("");
-                                }}
-                            >
-                                Delete
-                            </Button>
-                        )}
                     </div>
 
                     <Form.Item
                         name="first_name"
                         label={<p className="text-[#828ba2]">First Name</p>}
                     >
-                        <Input className="bg-[#eff2f9] border-none py-3 px-4" />
+                        <Input
+                            className="bg-[#eff2f9] border-none py-3 px-4"
+                            name="first_name"
+                        />
                     </Form.Item>
                     <Form.Item
                         name="last_name"
                         label={<p className="text-[#828ba2]">Last Name</p>}
                     >
-                        <Input className="bg-[#eff2f9] border-none py-3 px-4" />
+                        <Input
+                            className="bg-[#eff2f9] border-none py-3 px-4"
+                            name="last_name"
+                        />
                     </Form.Item>
                     <Form.Item
                         name="email"
                         label={<p className="text-[#828ba2]">Email</p>}
                     >
-                        <Input className="bg-[#eff2f9] border-none py-3 px-4" />
+                        <Input
+                            className="bg-[#eff2f9] border-none py-3 px-4"
+                            name="email"
+                        />
                     </Form.Item>
                     <Form.Item
                         name="phone"
                         label={<p className="text-[#828ba2]">Phone</p>}
                     >
-                        <Input className="bg-[#eff2f9] border-none py-3 px-4" />
+                        <Input
+                            className="bg-[#eff2f9] border-none py-3 px-4"
+                            name="phone"
+                        />
                     </Form.Item>
                     <Form.Item
                         name="country"
                         label={<p className="text-[#828ba2]">Country</p>}
                     >
-                        <Input className="bg-[#eff2f9] border-none py-3 px-4" />
+                        <Input
+                            className="bg-[#eff2f9] border-none py-3 px-4"
+                            name="country"
+                        />
                     </Form.Item>
                     <Form.Item
                         name="city"
                         label={<p className="text-[#828ba2]">City</p>}
                     >
-                        <Input className="bg-[#eff2f9] border-none py-3 px-4" />
+                        <Input
+                            className="bg-[#eff2f9] border-none py-3 px-4"
+                            name="city"
+                        />
                     </Form.Item>
                     <Form.Item
                         name="place_of_birth"
                         label={<p className="text-[#828ba2]">Place Of Birth</p>}
                     >
-                        <Input className="bg-[#eff2f9] border-none py-3 px-4" />
+                        <Input
+                            className="bg-[#eff2f9] border-none py-3 px-4"
+                            name="place_of_birth"
+                        />
                     </Form.Item>
                     <Form.Item
                         name="date_of_birth"
@@ -145,7 +166,10 @@ export default function PersonalDetails() {
                         tooltip="Add the date of birth only if it is a relevant requirement for your position. In most case , leave this blank"
                         label={<p className="text-[#828ba2]">Date Of Birth</p>}
                     >
-                        <Input className="bg-[#eff2f9] border-none py-3 px-4" />
+                        <Input
+                            className="bg-[#eff2f9] border-none py-3 px-4"
+                            name="date_of_birth"
+                        />
                     </Form.Item>
                 </div>
                 <Form.Item>
